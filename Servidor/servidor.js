39a79d1671d1e2f7968d6138982aeb26dfbb28f6
GiZanelato <giovanna.zanelato03@gmail.com>
2025-04-15 16:20:09 -0300

    15/4

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+require("colors");
+var http = require("http");
+var express = require("express");
+
+var app = express();
+app.use(express.static('./public'));
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log("Servidor rodando ...".rainbow);

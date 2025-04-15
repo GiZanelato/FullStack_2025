b5fe0ca7d255d107089362ac624926dc87f96ed6
GiZanelato <giovanna.zanelato03@gmail.com>
2025-04-15 14:34:44 -0300

    15/04

--- Aula 8/lab 8/script.js
+++ Aula 8/lab 8/script.js
@@ -1,28 +1,47 @@
-let dog = {
-    x: 0,
-    y: 100,
-    raio: 50,
-    img: new Image(),
-    desenha: function(){
-        this.img.src = 'imagens/dog.png';
-        ctx.beginPath();
-        ctx.drawImage(this.img, this.x, this.y, 2*this.raio, 2*this.raio);
-        ctx.closePath();
-    }
-}
+let canvas = document.getElementById('canvas1');
+let ctx = canvas.getContext('2d');
 
+// imagem
+let dog = new Image();
+dog.src = '../lab 8/imagens/dog.png';
 
-function animacao(){
-    ctx.clearRect(0,0,400,400)
-    bola.desenha();
-    requestAnimationFrame(animacao)
+// Posição inicial da imagem
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+
+// Tamanho da imagem
+const imgWidth = 30;
+const imgHeight = 30;
+
+dog.onload = function () {
+    // animação
+    animacao();
+
+    // Atualiza posição com o mouse
+    document.addEventListener('mousemove', function (evento) {
+        let rect = canvas.getBoundingClientRect();
+        let mouseX = evento.clientX - rect.left;
+        let mouseY = evento.clientY - rect.top;
+
+        // Centraliza imagem no ponteiro
+        x = mouseX;
+        y = mouseY;
+
+        // Garante que a imagem não ultrapasse os limites 
+        if (x - imgWidth / 2 < 0) x = imgWidth / 2;
+        if (y - imgHeight / 2 < 0) y = imgHeight / 2;
+        if (x + imgWidth / 2 > canvas.width) x = canvas.width - imgWidth / 2;
+        if (y + imgHeight / 2 > canvas.height) y = canvas.height - imgHeight / 2;
+
+        animacao();
+    });
+};
+
+function animacao() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let drawX = x - imgWidth / 2;
+    let drawY = y - imgHeight / 2;
+
+    ctx.drawImage(dog, drawX, drawY, imgWidth, imgHeight);
 }
-animacao();
-document.addEventListener('mousemove',function(evento){
-    let rect = canvas.getBoundingClientRect();
-    let x_mouse = evento.clientX - rect.left;
-    let y_mouse = evento.clientY - rect.top;
-    console.log(x_mouse,y_mouse);
-    bola.x = x_mouse;
-    bola.y = y_mouse;
-})
